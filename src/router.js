95173c34c18ca6952682b7ab2777d88c67147966
Jorge Escamilla <corem96@gmail.com>
2019-10-02 00:01:56 -0600

    feat: actualiza rutas y verifica logeo

--- src/router.js
+++ src/router.js
@@ -1,16 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './components/login/Login.vue'
+/*jshint esversion: 6 */
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './components/login/Login.vue';
+import Perfil from './components/usuario/perfil/Perfil.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+export const router = new Router({
   mode: 'history',
   // base: process.env.BASE_URL,
   routes: [
     { path: '/', redirect: { name: 'home' } },
     { path: '/home', name: 'home', component: Home },
     { path: '/login', name: 'Login', component: Login },
+    { path: '/usuario', component: Perfil },
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const login = ['/login'];
+  const auth = !login.includes(to.path);
+  const logeado = localStorage.getItem('usuario');
+
+  if (auth && !logeado) {
+    return next({
+      path: '/login',
+    });
+  }
+
+  next();
+});
