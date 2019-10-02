098ac3dbe437ecbfb892e43e3a449796e4bf310d
Jorge Escamilla <corem96@gmail.com>
2019-10-02 00:04:12 -0600

    feat: crea componente de perfil de usuario

---
+++ src/components/usuario/perfil/Perfil.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <div class="col-md-3">
+      <ul class="list-group pull-right">
+        <li class="list-group-item">Registrar domicilio</li>
+        <li class="list-group-item">Registrar tarjeta</li>
+        <li class="list-group-item">Cobranza</li>
+        <li class="list-group-item">Enviar recordatorio</li>
+        <li class="list-group-item">
+          <router-link to="/login">Logout</router-link>
+        </li>
+      </ul>
+    </div>
+
+    <div class="col-md-3">
+      <h3>Bienvenido {{ usuario.nombre }}</h3>      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {}
+    };
+  },
+  created() {
+    this.usuario = JSON.parse(localStorage.getItem('usuario'));
+  }
+}
+</script>
