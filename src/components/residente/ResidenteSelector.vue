e314e4a81b0065674e294df9efd3a4e113944de7
Jorge Escamilla <corem96@gmail.com>
2019-10-06 02:33:27 -0600

    feat: crea selector de residentes

---
+++ src/components/residente/ResidenteSelector.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-select label="nombre" :reduce="residente => residente.id"
+    :options="residentes" v-model="residenteId" @input="setSelected"></v-select>
+</template>
+
+<script>
+import Vue from 'vue';
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+
+Vue.component('v-select', vSelect);
+export default {
+  data() {
+    return {
+      residentes: [],
+      residenteId: 0
+    };
+  },
+  methods: {
+    setSelected(value) {
+      console.log(value);
+      this.residente = value;
+    }
+  }
+}
+</script>
