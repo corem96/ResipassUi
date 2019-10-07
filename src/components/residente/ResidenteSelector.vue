a27474f2ed8689d9a24dedf4926656b0e4c9ddef
Jorge Escamilla <corem96@gmail.com>
2019-10-07 00:23:41 -0600

    feat: mejora métodos de componente

--- src/components/residente/ResidenteSelector.vue
+++ src/components/residente/ResidenteSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-select label="nombre" :reduce="residente => residente.id"
+  <v-select label="nombreCompleto" :reduce="residente => residente.id"
     :options="residentes" v-model="residenteId" @input="setSelected"></v-select>
 </template>
 
@@ -7,6 +7,8 @@
 import Vue from 'vue';
 import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
+import { servicioUsuario } from '../../servicios/usuario/ServicioUsuario';
+import { servicioResidente } from '../../servicios/residente/ServicioResidente';
 
 Vue.component('v-select', vSelect);
 export default {
@@ -16,10 +18,21 @@ export default {
       residenteId: 0
     };
   },
+  created() {
+    const usuario = localStorage.getItem('usuario');
+    if (!usuario) { servicioUsuario.logout(); }
+
+    const residentes = servicioResidente.obtenerResidentes()
+      .then(datos => {
+        if (datos) {
+          this.residentes = datos;
+        }
+      });
+  },
   methods: {
     setSelected(value) {
-      console.log(value);
       this.residente = value;
+      // return value;
     }
   }
 }
