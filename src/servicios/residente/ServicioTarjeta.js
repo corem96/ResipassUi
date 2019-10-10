d0855a82ea46ffe79ba39bebd05672c508ee7a2c
Jorge Escamilla <corem96@gmail.com>
2019-10-10 01:45:41 -0600

    feat: crea servicio tarjeta para residentes

---
+++ src/servicios/residente/ServicioTarjeta.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6 */
+import { server } from '@/helper';
+import axios from 'axios';
+
+const residenteApi = `${server.baseUrl}/api/tarjeta`;
+
+async function obtenerTarjeta() {
+  try {
+    const resp = await axios.get(residenteApi);
+    if (resp) { return resp.data; }
+  } catch (error) {
+    if (error.response) {
+      const { data } = error.response;
+      return { error: `Ocurrió un error al intentar obtener los datos: ${data}`};
+    }
+  }
+}
+
+async function obtenerTarjetaResidente(residenteId) {
+  try {
+    const resp = await axios.get(`${residenteApi}/tarjeta-residente?residenteId=${residenteId}`);
+    if (resp) { return resp.data }
+  } catch (error) {
+    if (error.response) {
+      const { data } = error.response;
+      return { error: `Ocurrió un error al intentar obtener los datos: ${data}` };
+    }
+  }
+}
+
+export const servicioResidente = { obtenerTarjeta, obtenerTarjetaResidente };
